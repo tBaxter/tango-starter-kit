ffd37d5be2df8ecb7a30c660001eec87eab1032e
Tim Baxter <tim.baxter@cerner.com>
2013-08-02 22:40:01 -0500

    Installed test_db

--- project_name/settings.py
+++ project_name/settings.py
@@ -68,7 +68,7 @@ WSGI_APPLICATION = '{{ project_name }}.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'test_db',          # Or path to database file if using sqlite3.
+        'NAME': '{{ project_name }}/test_db',          # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
