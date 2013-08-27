aed6cc46179ddbfea2c725d6642cf693198db137
Tim Baxter <tim.baxter@cerner.com>
2013-08-27 10:46:39 -0500

    Removed allauth reference

--- project_name/urls.py
+++ project_name/urls.py
@@ -11,7 +11,6 @@ urlpatterns = patterns(
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^accounts/', include('allauth.urls')),
     url(r'^articles/', include('articles.urls.article_urls')),
     url(r'^comments/', include('django.contrib.comments.urls')),
     url(r'^contact/', include('contact_manager.urls')),
