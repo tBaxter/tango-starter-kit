7f78cf0669503465f924e2e28fc778019fa025dc
Tim Baxter <tim.baxter@cerner.com>
2014-08-08 11:59:16 -0500

    Cleanup on settings

--- project_name/settings.py
+++ project_name/settings.py
@@ -6,21 +6,20 @@ https://docs.djangoproject.com/en/{{ docs_version }}/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/{{ docs_version }}/ref/settings/
+
+These are quick-start development settings. They are unsuitable for production.
+See https://docs.djangoproject.com/en/{{ docs_version }}/howto/deployment/checklist/
+
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 from tango_shared.settings import *
 
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+PROJECT_NAME = '{{ project_name }}'
 PROJECT_DIR = os.path.dirname(os.path.realpath(__file__))
 WEBAPP_DIR  = os.path.abspath(os.path.join(PROJECT_DIR, os.path.pardir))
 PARENT_DIR  = os.path.abspath(os.path.join(WEBAPP_DIR, os.path.pardir))
-
-PROJECT_NAME = '{{ project_name }}'
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/{{ docs_version }}/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -29,21 +28,29 @@ if DEBUG is True:
     THUMBNAIL_DEBUG = True
     from tango_shared.debug_settings import *
 
-SITE_ID = 1
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '{{ secret_key }}'
-
+# CORE DJANGO SETTINGS
+# https://docs.djangoproject.com/en/{{ docs_version }}/ref/settings/#core-settings
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
 )
-MANAGERS = ADMINS
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = []
 
-# Application definition
+# https://docs.djangoproject.com/en/{{ docs_version }}/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '{{ project_name }}/test_db',          # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
+    }
+}
+
 INSTALLED_APPS = (
     'tango_shared',
     'django.contrib.admin',
@@ -60,78 +67,66 @@ INSTALLED_APPS = (
 
 INSTALLED_APPS += TANGO_APPS
 
-LOGIN_URL = '/login/'
-
-ROOT_URLCONF = '{{ project_name }}.urls'
-
-WSGI_APPLICATION = '{{ project_name }}.wsgi.application'
-
-# https://docs.djangoproject.com/en/{{ docs_version }}/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '{{ project_name }}/test_db',          # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
-    }
-}
-
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'America/Chicago'
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
-# Internationalization
-# https://docs.djangoproject.com/en/{{ docs_version }}/topics/i18n/
-TIME_ZONE = 'UTC'
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
-USE_I18N = True
-# If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale.
-USE_L10N = True
-# If you set this to False, Django will not use timezone-aware datetimes.
-USE_TZ = True
+MANAGERS = ADMINS
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 MEDIA_ROOT = '%s/media/%s/' % (PARENT_DIR, PROJECT_NAME)
 
-# https://docs.djangoproject.com/en/1.5/howto/static-files/
-STATIC_ROOT = '%s/collected_static/%s/' % (PARENT_DIR, PROJECT_NAME)
-STATICFILES_DIRS = (PROJECT_DIR + "/static/",)
+ROOT_URLCONF = '{{ project_name }}.urls'
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '{{ secret_key }}'
+
+SITE_ID = 1
 
 TEMPLATE_DIRS = (
     PROJECT_DIR + '/templates'
 )
 
-AUTH_USER_MODEL  = 'user_profiles.Profile'
+WSGI_APPLICATION = '{{ project_name }}.wsgi.application'
 
+
+### AUTH SETTINGS
+### https://docs.djangoproject.com/en/dev/ref/settings/#auth
+
+AUTH_USER_MODEL  = 'user_profiles.Profile'
+LOGIN_URL = '/login/'
+
+
+### Session Settings
+### https://docs.djangoproject.com/en/dev/ref/settings/#sessions
+
+
+### Sites Settings
+### https://docs.djangoproject.com/en/dev/ref/settings/#sites
+SITE_ID = 1
+
+# https://docs.djangoproject.com/en/1.5/howto/static-files/
+STATIC_ROOT = '%s/collected_static/%s/' % (PARENT_DIR, PROJECT_NAME)
+STATICFILES_DIRS = (PROJECT_DIR + "/static/",)
+
+
+
+
+
+# Application definition
+
+
+
+
+### ADDITIONAL TANGO/SITE SETTINGS YOU MAY WANT TO SET OR OVERRIDE
+### https://github.com/tBaxter/Tango
 
 # Themes are any themes defined in the css/themes/ directory. 
 # You can easily create your own themes simply by
 # adding the css file to the themes directory and including them here in ALLOWABLE_THEMES.
 # If no DEFAULT_THEME is specified, 'site.css' will be used.
-ALLOWABLE_THEMES = ['default', 'dark', 'light', 'vert'] 
-DEFAULT_THEME = 'vert'
+# ALLOWABLE_THEMES = ['default', 'dark', 'light', 'vert'] 
+# DEFAULT_THEME = 'vert'
 
-# Sets default pagination count (number of items per page)
-PAGINATE_BY = 25 
-
-# Set google analytics ID 
-# and Google maps key.
-GOOGLE_ANALYTICS_ID = ''  # Google analytics GA code
-GMAP_KEY = ''  # Google Maps Key
-
-# if set to false, RESTRICT_CONTENT_TO_SITE will allow
-# sites/projects to share content.
-# If true, content will be limited to the current site.
-RESTRICT_CONTENT_TO_SITE = True
+# Google maps key and analytics GA code
+# GMAP_KEY = ''  # Google Maps Key
+# GOOGLE_ANALYTICS_ID = ''  # Google analytics GA code
 
 # If your site is a news source, give the name.
 # This will attach the name of your organization to articles
@@ -139,38 +134,21 @@ RESTRICT_CONTENT_TO_SITE = True
 # including options to mark content as
 # opinion/editorial, dateline, and noting another source as
 # the origin of the content.
-NEWS_SOURCE = ''
+# NEWS_SOURCE = False
+
+# PAGINATE_BY = 25 
+
+# if set to false, RESTRICT_CONTENT_TO_SITE will allow
+# sites/projects to share content.
+# If true, content will be limited to the current site.
+#RESTRICT_CONTENT_TO_SITE = True
+
+
+
+### APP SETTINGS: Provides overrides or defaults to these apps
 
 # Comment moderation settings
-COMMENTS_CLOSE_AFTER = 30  # Number of days after publication until comments close.
-COMMENTS_MOD_AFTER = 15  # Number of days after publication until comments require moderation. 
+#COMMENTS_CLOSE_AFTER = 30  # Number of days after publication until comments close.
+#COMMENTS_MOD_AFTER = 15  # Number of days after publication until comments require moderation. 
 
 
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error when DEBUG=False.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    }
-}
