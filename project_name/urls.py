de5f3014bccbc85ff08eb877ece25dafb41f6b57
Tim Baxter <baxter@gretschpages.com>
2018-06-28 14:15:31 -0500

    Updated urls for Django 2.0

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,60 +1,42 @@
 from django.conf import settings
-from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.urls import include, path, re_path
 from django.views.generic import TemplateView
+
+from tango_shared.views import build_howto
+from voting.views import generic_vote_on_object
+
 admin.autodiscover()
 
-urlpatterns = patterns(
-    '',
-    url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
-
-    url(
-        regex=r'^admin/how-to/$',
-        view='tango_shared.views.build_howto',
-        name="admin_how_to"
-    ),
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    url(r'^admin/', include(admin.site.urls)),
-
-    url(r'^articles/', include('articles.urls.article_urls')),
-    #url(r'^comments/', include('tango_comments.urls')),
-    url(r'^contact/', include('contact_manager.urls')),
-    url(r'^photos/', include('photos.urls')),
-    url(r'^events/', include('happenings.urls')),
+urlpatterns = [
+    path('', TemplateView, {'template_name': 'index.html'}, name='home'),
+    path('admin/how-to/', build_howto, name="admin_how_to"),
+    path('admin/doc/', include('django.contrib.admindocs.urls')),
+    path('admin/', include('admin.site.urls')),
+    path('articles/', include('articles.urls.article_urls')),
+    #path('comments/', include('tango_comments.urls')),
+    path('contact/', include('contact_manager.urls')),
+    path('photos/', include('photos.urls')),
+    path('events/', include('happenings.urls')),
     # Uncomment if you're using the tango_user app.
     # Or just use your own profiles app.
-    # url(r'^profiles/', include('tango_user.urls')),
-    url(r'^video/', include('video.urls')),
+    # path('profiles/', include('tango_user.urls')),
+    path('video/', include('video.urls')),
 
-    # VOTING
-    url(
-        name="generic_vote",
-        regex=r'^vote/(?P<model_name>[-\w]+)/(?P<object_id>\d+)/(?P<direction>up|down)vote/$',
-        view='voting.views.generic_vote_on_object'
+    re_path(
+        route=r'^vote/(?P<model_name>[-\w]+)/(?P<object_id>\d+)/(?P<direction>up|down)vote/$',
+        view=generic_vote_on_object,
+        name="generic_vote"
     ),
 
     # Map these urls to appropriate login/logout views for your authentication system.
-    #url(
-    #    r'^login/$',
-    #    auth_views.login,
-    #    {'template_name': 'registration/login.html'},
-    #    name='auth_login'
-    #),
-    #url(
-    #    r'^logout/$',
-    #    auth_views.logout,
-    #    {'template_name': 'registration/logout.html'},
-    #    name='auth_logout'
-    #),
+    #path('login/', auth_views.login, {'template_name': 'registration/login.html'}, name='auth_login'),
+    #path('logout/', auth_views.logout, {'template_name': 'registration/logout.html'}, name='auth_logout'),
 
     # This url is just to provide example typography and show the typographic grid.
-    url(
-        regex='^examples/typography/$',
-        view=TemplateView.as_view(template_name='examples/typography.html'),
-        name='typography'
-    ),
-)
+    path('examples/typography/', TemplateView, {'template_name': 'examples/typography.html'}, name='typography'),
+]
 
 if settings.DEBUG is True:
     from django.conf.urls.static import static
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns.append(static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
